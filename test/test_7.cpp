ecb9b98d523c3d4bf05879e28144bcc860c8d331
Adrian Brodzik <adrianbrodzyk@gmail.com>
2019-03-09 19:18:36 +0100

    Fix method name

--- test/test_7.cpp
+++ test/test_7.cpp
@@ -5,17 +5,17 @@ TEST_CASE("set column")
 {
     BitMatrix bm(6, 14);
 
-    bm.setCol(0, 511);
-    bm.setCol(1, 0);
-    bm.setCol(2, 1574);
-    bm.setCol(3, 16383);
-    bm.setCol(4, 8192);
-    bm.setCol(5, 9135);
+    bm.setColumn(0, 511);
+    bm.setColumn(1, 0);
+    bm.setColumn(2, 1574);
+    bm.setColumn(3, 16383);
+    bm.setColumn(4, 8192);
+    bm.setColumn(5, 9135);
 
-    CHECK(bm.getCol(0) == 511);
-    CHECK(bm.getCol(1) == 0);
-    CHECK(bm.getCol(2) == 1574);
-    CHECK(bm.getCol(3) == 16383);
-    CHECK(bm.getCol(4) == 8192);
-    CHECK(bm.getCol(5) == 9135);
+    CHECK(bm.getColumn(0) == 511);
+    CHECK(bm.getColumn(1) == 0);
+    CHECK(bm.getColumn(2) == 1574);
+    CHECK(bm.getColumn(3) == 16383);
+    CHECK(bm.getColumn(4) == 8192);
+    CHECK(bm.getColumn(5) == 9135);
 }
